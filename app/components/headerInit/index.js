63e21a963dc7e02585f03efabda550b1e0caa27a
Jordi Garreta <jordigarreta11@gmail.com>
2023-10-03 09:40:51 +0200

    add js

---
+++ app/components/headerInit/index.js
@@ -0,0 +1,21 @@
+'use client'
+
+import styles from './headerInit.module.scss'
+import { motion } from 'framer-motion'
+
+const transition = { delay: 0, duration: 1, ease: [0.43, 0.13, 0.23, 0.96] };
+
+export default function  headerInit() {
+    return(
+        <header className={styles.header}>
+            <motion.img
+                initial={{ translateY: -20, opacity: 0 }}
+                animate={{ translateY: 0, opacity: 1 }}
+                exit={{ translateY: -20, opacity: 0 }}
+                transition={transition}
+                className={styles.header_img}
+                src="/respublica--logo.svg" alt="Respublica Logo"
+            />
+        </header>
+    )
+}
